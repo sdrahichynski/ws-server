d58567753f7f9f8b43c4fad721f5e38c8c5c02ff
sdrahichynski <sdrahichynski@aristeksystems.com>
2022-10-31 16:00:32 +0300

    feat: frontend

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import Koa from "koa";
 import http from "node:http";
 import { Server } from "socket.io";
+import serve from "koa-static";
 
 const app = new Koa();
 const httpServer = http.createServer(app.callback());
 const io = new Server(httpServer, { cors: { origin: "http://localhost:3000" } });
 
 let field = null;
+
+app.use(serve('./frontend'));
 
 io.on("connection", (socket) => {
     socket.emit("NEW_VALUE", field);
