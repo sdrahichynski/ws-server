c6af2b1d9ec3a5af247a99a2e3c2611989a4be79
sdrahichynski <sdrahichynski@aristeksystems.com>
2022-11-04 22:28:03 +0300

    fix: fast updates

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ import serve from "koa-static";
 
 const app = new Koa();
 const httpServer = http.createServer(app.callback());
-const io = new Server(httpServer, { cors: { origin: "*" } });
-
-let field = null;
-
 app.use(serve('./frontend'));
 
-io.on("connection", (socket) => {
+const SIZE = 48;
+let field = new Array(SIZE * SIZE).fill("#fff");
+const io = new Server(httpServer, { cors: { origin: "*" } });
+const gameIo = io.of("/game");
+const chatIo = io.of("/chat");
+
+gameIo.on("connection", (socket) => {
     socket.emit("NEW_VALUE", field);
 
-    socket.on("UPDATE_VALUE", (data) => {
-        field = data;
-        socket.broadcast.emit("NEW_VALUE", data);
+    socket.on("UPDATE_VALUE", ({ index, color }) => {
+        field[index] = color;
+        socket.broadcast.emit("NEW_VALUE", field);
     })
 });
 
+chatIo.on("connection", (socket) => {
+
+});
 
 
 httpServer.listen(3001, () => {
